cf1fa6f02d42bc1f297ab9e45782ec03d1e2cb88
4ortytwo <arthur.burgan@spronq.com>
2020-08-16 01:18:59 +0200

    initial commit

---
+++ src/features/authentication/onboarding/Subslide.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import Button from "../../Button";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 44,
+  },
+  subtitle: {
+    fontFamily: "SFProText-Semibold",
+    fontSize: 24,
+    lineHeight: 30,
+    marginBottom: 12,
+    color: "#0C0D34",
+    textAlign: "center",
+  },
+  description: {
+    fontFamily: "SFProText-Regular",
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 40,
+    color: "#0C0D34",
+    textAlign: "center",
+  },
+});
+
+interface SubslideProps {
+  subtitle: string;
+  description: string;
+  last?: boolean;
+  onPress: () => void;
+}
+
+const Subslide = ({ subtitle, description, last, onPress }: SubslideProps) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.subtitle}>{subtitle}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <Button
+        label={last ? "Let's get started" : "Next"}
+        variant={last ? "primary" : "default"}
+        {...{ onPress }}
+      />
+    </View>
+  );
+};
+
+export default Subslide;
